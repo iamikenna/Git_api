60a187e349275fcc97b56dea7a0be081ffcd5b46
iamikenna <45284274+iamikenna@users.noreply.github.com>
2020-02-25 09:23:55 -0500

    Add files via upload

---
+++ HW_04a_Ikenna_IbezimAPI_test_2.py
@@ -0,0 +1,32 @@
+"""
+Author: Ikenna Ibezim
+Write a program to classify triangles
+Test cases for HW1
+"""
+import unittest
+from unittest import mock
+from HW_04_Ikenna_Ibezim_api import github_api
+from unittest.mock import Mock, patch
+from nose.tools import assert_is_none, assert_list_equal
+import HW_04_Ikenna_Ibezim_api
+
+
+class TestGetRepo(unittest.TestCase):
+
+    @mock.patch('HW_04_Ikenna_Ibezim_api.github_api')
+    def test_github_api(self, mock_simple_func):
+        # print(mock_simple_func)
+        # print(HW_04_Ikenna_Ibezim_api.github_api)
+        seen = github_api()
+        expected =['User: iamikenna', 'Repo: Employee-Appraisal-system Number of commits: 1', 'Repo: Git_api Number of commits: 12', 'Repo: SE_Testing Number of commits: 2', 'Repo: Stevens_Dev Number of commits: 3', 'Repo: tic-tac-toe Number of commits: 1', 'Repo: Triangle_testing Number of commits: 11']
+        mock_simple_func.return_value = seen
+        result = HW_04_Ikenna_Ibezim_api.github_api()
+        self.assertEqual(result, expected)
+      
+ 
+        
+if __name__ == "__main__":
+    unittest.main(exit=False, verbosity=2)
+   
+   
+   
