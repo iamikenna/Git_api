3cbfa59fc8aba566fe450c3cb051df107e850532
iamikenna <45284274+iamikenna@users.noreply.github.com>
2020-02-26 09:38:03 -0500

    Add files via upload

--- HW_04a_Ikenna_IbezimAPI_test_2.py
+++ HW_04a_Ikenna_IbezimAPI_test_2.py
@@ -12,19 +12,20 @@ import HW_04_Ikenna_Ibezim_api
 
 
 class TestGetRepo(unittest.TestCase):
-
+    """Mocking the git_hub function in order to reduce its dependency"""
     @mock.patch('HW_04_Ikenna_Ibezim_api.github_api')
     def test_github_api(self, mock_simple_func):
-        # print(mock_simple_func)
-        # print(HW_04_Ikenna_Ibezim_api.github_api)
-        seen = github_api()
         expected =['User: iamikenna', 'Repo: Employee-Appraisal-system Number of commits: 1', 'Repo: Git_api Number of commits: 15', 'Repo: SE_Testing Number of commits: 2', 'Repo: Stevens_Dev Number of commits: 3', 'Repo: tic-tac-toe Number of commits: 1', 'Repo: Triangle_testing Number of commits: 11']
-        mock_simple_func.return_value = seen
+        mock_simple_func.return_value = "you have mocked it"
         result = HW_04_Ikenna_Ibezim_api.github_api()
-        self.assertEqual(result, expected)
-      
- 
+        self.assertEqual(result, "you have mocked it")
         
+        mock_simple_func.return_value = github_api()
+        result2 = HW_04_Ikenna_Ibezim_api.github_api()
+        
+        self.assertEqual(result2, expected)
+        self.assertNotEqual(result2, 'unable to fetch repos from user')
+        print(result, result2)
 if __name__ == "__main__":
     unittest.main(exit=False, verbosity=2)
    
